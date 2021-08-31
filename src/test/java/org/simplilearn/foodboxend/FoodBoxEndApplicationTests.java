7832a6d3e0d68f8a8c7b845a267c00de28f9ccb0
Toppar <toppar_bourmaf@dell.com>
2021-08-31 12:08:29 -0500

    first commit

---
+++ src/test/java/org/simplilearn/foodboxend/FoodBoxEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.simplilearn.foodboxend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodBoxEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
